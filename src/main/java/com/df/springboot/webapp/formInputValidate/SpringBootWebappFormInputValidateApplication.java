df2ac6fdbaba3015efaef280375232f34748697f
gtodur <gtodur@outlook.com>
2020-03-14 10:25:12 +0530

    First commit

---
+++ src/main/java/com/df/springboot/webapp/formInputValidate/SpringBootWebappFormInputValidateApplication.java
@@ -0,0 +1,13 @@
+package com.df.springboot.webapp.formInputValidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebappFormInputValidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebappFormInputValidateApplication.class, args);
+	}
+
+}
